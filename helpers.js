5d5b8ca7466dbb479c0a55e2e6b16605fe22a317
dyiar <austin.hl.blake@gmail.com>
2019-01-17 16:40:08 -0800

    reconfigured helper.js file

--- helpers.js
+++ helpers.js
@@ -1,55 +1,25 @@
 const db = require('./dbconfig');
 
 module.exports = {
-    getDishes: function(req, res) {
-        db('dish')
-        .then(dishes => {
-            res.status(200).send({ dishes })
-        })
-        .catch(() => res.status(500).send({ error: "The data could not be retrieved." }))
+    getDishes: function() {
+        return db.select().from('dish');
     },
 
     addDish: function(dish) {
-        db('dish')
-        .insert(dish)
-        .then(ids => {
-            db('dish')
-            .where({ id: ids[0] })
-            .then(newDish => {
-                res.status(201).send({ newDish })
-            })
-        })
-        .catch(() => res.status(500).send({ error: "Data couldn't be saved." }))
+        return db.insert(dish).into('dish').then(ids => { id: ids[0]})
     },
 
     getDish: function(id) {
-            db('dish')
-            .where({ id })
-            .then(dishes => {
-                res.status(200).send({ dishes })
-            })
-            .catch(() => res.status(500).send({ error: "The data could not be retrieved." }))
+            return db.select().where({ id }).from('dish')
 
     },
 
     getRecipes: function() {
-        db.select('recipes.id', 'recipe.name', 'dish.id as dish').from('recipes').innerJoin('dish', 'dish.id', '=', 'recipe.dish_id').where('recipe.id', id).then(response => {
-            res.status(200).json({ response })
-        })
-        .catch(() => res.status(500).send({ error: "The data couldn't be retrieved." }))
+        return db.select('recipes.id', 'recipe.name', 'dish.id as dish').from('recipes').innerJoin('dish', 'dish.id', '=', 'recipe.dish_id').where('recipe.id', id)
     },
 
     addRecipe: function(recipe) {
-        db('recipe')
-        .insert(recipe)
-        .then(ids => {
-            db('recipe')
-            .where({ id: ids[0] })
-            .then(newRecipe => {
-                res.status(201).send({ newRecipe })
-            })
-        })
-        .catch(() => res.status(500).send({ error: "Data couldn't be saved." }))
+        return db.insert(recipe).into('recipes').then(ids => {id: ids[0] })
     }
 
 }
